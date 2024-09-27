8752c60411720e42d45b6ec776c25d973ef74aff
khadiza21 <bibikhadiza474@gmail.com>
2024-09-27 12:52:37 +0600

    home page made by html css js

--- JavaScript/Home.js
+++ JavaScript/Home.js
@@ -1,12 +1,25 @@
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 4,
-  spaceBetween: 30,
+  slidesPerView: 2,
+  spaceBetween: 10,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+  },
 });
-
 
 
 // mobile nav
