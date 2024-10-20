a46408080538fa0d137329eb68a782ce30082f2a
khadiza21 <bibikhadiza474@gmail.com>
2024-10-20 21:32:28 +0600

    master mall update

--- JavaScript/scrollReveal.js
+++ JavaScript/scrollReveal.js
@@ -30,3 +30,17 @@ ScrollReveal().reveal(".footer", {
   duration: 1000,
   delay: 100,
 });
+
+
+// contact page
+// ScrollReveal animations
+ScrollReveal().reveal('.about-header', { delay: 200, duration: 1200 });
+ScrollReveal().reveal('.details', { delay: 200, duration: 1200, origin: 'left', distance: '50px' });
+ScrollReveal().reveal('.map', { delay: 200, duration: 1200, origin: 'right', distance: '50px' });
+ScrollReveal().reveal('.form-container', { delay: 200, duration: 1200, origin: 'bottom', distance: '50px' });
+
+// Form validation
+document.querySelector('#contact-form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert('Form submitted successfully!');
+});
