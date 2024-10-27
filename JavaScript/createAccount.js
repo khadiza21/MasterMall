d297ef80cdffe259463f0278ead0631226dee304
khadiza21 <bibikhadiza474@gmail.com>
2024-10-27 20:27:38 +0545

    sign up/sign in  page

--- JavaScript/createAccount.js
+++ JavaScript/createAccount.js
@@ -5,3 +5,6 @@ const Nav = document.querySelector(".mobile_nav");
 hamburger.addEventListener("click", () => {
   Nav.classList.toggle("mobile_nav_hide");
 });
+
+// ScrollReveal animations
+ScrollReveal().reveal('.auth_container', { delay: 200, duration: 1200, origin: 'bottom' });
