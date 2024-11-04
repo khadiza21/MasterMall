44411db44bac2459548de043aca35247037a6514
khadiza21 <bibikhadiza474@gmail.com>
2024-11-04 23:45:38 +0545

    checkout page

--- JavaScript/checkout.js
+++ JavaScript/checkout.js
@@ -1,15 +1,6 @@
-// checkout page
-const hamburger = document.querySelector(".hamburger");
-const Nav = document.querySelector(".mobile_nav");
-
-hamburger.addEventListener("click", () => {
-  Nav.classList.toggle("mobile_nav_hide");
-});
-//
 document.addEventListener("DOMContentLoaded", function () {
-  // Retrieve cart data from local storage
-  const cartData = JSON.parse(localStorage.getItem("cart")) || [];
 
+  const cartData = JSON.parse(localStorage.getItem("cart")) || [];
   const cartItemsContainer = document.querySelector(".cart-items");
   const shippingPrice = document.querySelector(".shipping-cost");
   const subTotalPrice = document.querySelector(".sub-total-items");
@@ -21,17 +12,17 @@ document.addEventListener("DOMContentLoaded", function () {
   let shippingCost = 20;
 
   cartData.forEach((item) => {
-    // Calculate total items and total price
+
     totalItems += item.quantity;
     totalPrice += item.price * item.quantity;
 
-    // Create cart item element
+
     const cartItem = document.createElement("div");
     cartItem.classList.add("summary-item");
 
     cartItem.innerHTML = `
 
-    <div class="summary-item-details">
+              <div class="summary-item-details">
                   <div class="summary-item-img">
                     <img src="${item.image}" alt="${item.title}">
                      <p> ${item.quantity}</p>
@@ -42,18 +33,17 @@ document.addEventListener("DOMContentLoaded", function () {
                     <p class="item-color">${item.color}</p>
                   </div>
               </div>
-                  <div>
-                    <span class="item-price">$${(item.price * item.quantity).toFixed(2)}</span>
-                  </div>
+              <div>
+                <span class="item-price">$${(item.price * item.quantity).toFixed(2)}</span>
+              </div>
       `;
 
-    // Append cart item to cart items container
     cartItemsContainer.appendChild(cartItem);
   });
 
-  // Update total items and total price elements
+
   subTotalPrice.textContent = `$ ${totalPrice.toFixed(2)}`;
   shippingPrice.textContent = `$ ${shippingCost.toFixed(2)}`;
   totalItemsElement.textContent = totalItems;
-  totalPriceElement.textContent = ` $ ${(totalPrice + shippingCost).toFixed(2)}`; // Including $20 shipping fee
+  totalPriceElement.textContent = ` $ ${(totalPrice + shippingCost).toFixed(2)}`;
 });
